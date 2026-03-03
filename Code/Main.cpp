b74f11843531d2a57d2e040d15276245bf96dcc4
Muneeb Rehman <muneebbhanbhro9@gmail.com>
2026-03-03 13:43:57 +0500

    Add Budget class and implement singleton for BudgetManager
    Added Budget class with name and limit attributes, and a method to add transactions. Implemented singleton pattern for BudgetManager class.

--- Code/Main.cpp
+++ Code/Main.cpp
@@ -33,16 +33,51 @@ class Category
 		
 };
 
-class BudgetManager
+class Budget // for multiple budgets
 {
 	private:
-		double limit;  // total budget
+		std::string name;
+		double limit;
 //		double daily;  // a daily spending limit calculated using limit and budget duration, exceeding this will cause it  to be recalculated
+//		int duration  // to-do
 		std::vector <Transactions*> transactionVctr;  // a vector to store the list of pointers to transactions
 		
 	public:
+		Budget(std::string name, double limit) : name(name), limit(limit) {}
+		
 		void addTransaction(Transactions* t)
 		{
 			transactionVctr.push_back(t);
 		}
 };
+
+/* the singleton
+excerpt from Refactoring Guru:
+All implementations of the Singleton have these two steps in common:
+
+    1. Make the default constructor private, to prevent other objects from using the new operator with the Singleton class.
+    2. Create a static creation method that acts as a constructor. Under the hood, this method calls the private constructor to create an object and saves it in a static field. All following calls to this method return the cached object.
+
+If your code has access to the Singleton class, then it’s able to call the Singleton’s static method. So whenever that method is called, the same object is always returned. */
+
+class BudgetManager
+{
+	private:
+		BudgetManager();
+		static BudgetManager& getInstance()
+		{
+			BudgetManager instance;
+			return instance;
+		}
+		std::vector <Budget*> budgetVctr;
+		
+	public:
+};
+
+int main()
+{
+	std::cout << "\n-------- FinaLT --------\n";
+	
+	BudgetManager manager;
+	
+}
